83a0d40c96e79a52401f36670c37988b2b69701b
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2022-11-26 20:43:26 +0630

    initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+### Basketball Teams App
+
+React app with complete routing system for learning purpose
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Basketball League</div>;
+};
+
+export default App;
