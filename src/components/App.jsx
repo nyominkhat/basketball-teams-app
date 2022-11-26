6cf50f86f41382d76256edcaeca41b463856c541
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2022-11-26 20:56:10 +0630

    basic route setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Players from "./Players";
+import Teams from "./Teams";
 
 const App = () => {
-  return <div>Basketball League</div>;
+  return (
+    <>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/players" element={<Players />} />
+            <Route path="/teams" element={<Teams />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="container navbar">
+      <Link to="/">Home</Link>
+
+      <nav className="nav-links">
+        <Link to="/players">Players</Link>
+        <Link to="/teams">Teams</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
